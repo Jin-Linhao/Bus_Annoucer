6b4f71478e2d562f3c9322a5a8206d39bd775f2b
Jin-Linhao <jin_linhao@hotmail.com>
2016-03-22 23:13:35 +0800

    first GUI

--- script/bluetooth_scanner.py
+++ script/bluetooth_scanner.py
@@ -17,72 +17,90 @@ from urllib2 import urlopen, URLError
 import pygame.mixer
 from pygame.locals import *
 import cv2
+import Tkinter as tk
+
+
 # get the name of this scanner
 scanner_id = os.popen('uname -n').readline().strip()
-
+time_end=0
+root = tk.Tk()
+v = tk.StringVar()
 
 id_dict = {"Linhao's":["02.wav", "Bus179"], 
-           "Edina":["02.wav", "Bus199"],
-           "Galaxy":["02.wav", "Bus179A"],
-           "Guoyong's":["02.wav", "Bus123"]}
+		   "Edina":["02.wav", "Bus199"],
+		   "Galaxy":["02.wav", "Bus179A"],
+		   "Guoyong's":["02.wav", "Bus123"]}
 
 
 
 
 def scan_id():
-    """Scan nearby bluetooth devices and get all the bluetooth name"""
-    ids = []
+	"""Scan nearby bluetooth devices and get all the bluetooth name"""
+	ids = []
 
-    # launch the scanner
-    f = os.popen('hcitool scan --length=2')
-    # get the output from the scanner utility
-    unparsed_data = f.readlines()[:]
+	# launch the scanner
+	f = os.popen('hcitool scan --length=2')
+	# get the output from the scanner utility
+	unparsed_data = f.readlines()[:]
 
-    for u in unparsed_data:
-        # get the ID of the bluetooth devices
-        # test = u.split()[1]
-        # print test
-        for id in id_dict.keys():
-        	# print id_dict.keys()
-        	if id  == u.split()[1]:
-        		ids.append(id)
-        else:
-        	pass
+	for u in unparsed_data:
+		# get the ID of the bluetooth devices
+		# test = u.split()[1]
+		# print test
+		for id in id_dict.keys():
+			# print id_dict.keys()
+			if id  == u.split()[1]:
+				ids.append(id)
+		else:
+			pass
 
-    return ids
+	return ids
 
 
-time_end=0
+
 def show_id():
-    """print the bluetooth id on screen and broadcast the audio recording"""
-    global time_end
-
-    ids = scan_id()
-    for id in ids:
-    	print id_dict[id][1]
-        if pygame.mixer.get_init() and time.time()>time_end:
-	        audio_record = pygame.mixer.Sound("/home/eee/Documents/Bus_Annoucer/audio/" + id_dict[id][0])
-	        audio_player = audio_record.play(maxtime=2000) #play the sound for two seconds
-	        img = cv2.imread("/home/eee/Desktop/bus179.png")
-	        time_end = time.time() + 2
-	        #cv2.imshow("Bus179", img)
-	        #cv2.waitKey(1000) 
+	"""print the bluetooth id on screen and broadcast the audio recording"""
+	global time_end
+	
 
 
+	ids = scan_id()
 
-		        # pass
-		        # print id_dict[id][1]
-        
+	for id in ids:
+		print id_dict[id][1]
+		if pygame.mixer.get_init() and time.time()>time_end:
+			audio_record = pygame.mixer.Sound("/home/eee/Documents/Bus_Annoucer/audio/" + id_dict[id][0])
+			audio_player = audio_record.play(maxtime=2000) #play the sound for two seconds
+			time_end = time.time() + 2
+			v.set(id_dict[id][1])
+
+			text = tk.Label(root, textvariable=v, font=("Helvetica", 20), width = 160, height = 100).pack()
+			
+			
+	# root.mainloop()	 
+	# time_end = time.time() + 2
+	# root.destroy()
+
+
+
+				# pass
+				# print id_dict[id][1]
+		
 
 
 
 if __name__ == '__main__':
 
-    pygame.mixer.init()
+	pygame.mixer.init()
 
-    while True:
-        print "..."
-        # continuously scan the world for new devices 
-        show_id()
+	while True:
+		print "..."
+		# continuously scan the world for new devices
 
-    
+		
+		show_id()
+		root.mainloop()	 	
+		
+
+
+	
